51f59fc84beb4f3154f6cc1b04914078c8696524
Itiel Dario <itiel.dario@gmail.com>
2026-03-24 14:48:43 -0300

    feat: containerize application

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,6 @@ import app from "./app.js"
 
 const PORT: number = Number(process.env.PORT_BACKEND) || 5000
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`API rodando em http://localhost:${PORT}`)
 })
